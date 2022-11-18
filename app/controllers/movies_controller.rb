35899f6a5a7de8ce0b740e9b984dfb6b4f7e0ffe
Victor Rubia <victorrubia@correo.ugr.es>
2022-11-18 10:18:59 +0100

    Defino funciones para eliminar las películas close #1 #23

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -32,6 +32,11 @@ class MoviesController < ApplicationController
     end
   end
 
+  # DELETE /sessions/1.json
+  def destroy
+    @movie.destroy
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_movie
