9440f576781c5141f2e66930c5edf3150a4c6b0d
Victor Rubia <victorrubia@correo.ugr.es>
2022-12-05 22:27:29 +0100

    Desarrollo la funcionalidad para autenticación close #45

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: :user_sessions },
+             path_names: { sign_in: :login }
+  resource :user, only: [:show, :update]
+  resources :movies
+  resources :sessions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   #
   # root "movies#index"
-  resources :movies
-  resources :sessions
   get 'cartelera', to: 'sessions#get_cartelera'
 end
