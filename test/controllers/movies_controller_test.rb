b07de217d41fd2de5cd09707783f30ff98f4df16
Victor Rubia <victorrubia@correo.ugr.es>
2022-12-14 12:21:50 +0100

    Desarrollo de pruebas para testear la funcionalidad de autorrelleno #10 close #55

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -13,7 +13,15 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create movie" do
     assert_difference("Movie.count") do
-      post movies_url, params: { movie: { age_classification: @movie.age_classification, director: @movie.director, genre: @movie.genre, launch_date: @movie.launch_date, length: @movie.length, poster: @movie.poster, rating_avg: @movie.rating_avg, synopsis: @movie.synopsis, title: @movie.title } }, as: :json, headers: { "HTTP_AUTHORIZATION" => "Token token=#{@user}" }
+      post movies_url, params: {  age_classification: @movie.age_classification, director: @movie.director, genre: @movie.genre, launch_date: @movie.launch_date, length: @movie.length, poster: @movie.poster, rating_avg: @movie.rating_avg, synopsis: @movie.synopsis, title: @movie.title }, as: :json, headers: { "HTTP_AUTHORIZATION" => "Token token=#{@user}" }
+    end
+
+    assert_response 201
+  end
+
+  test "should create known movie without data" do
+    assert_difference("Movie.count") do
+      post movies_url, params: {  title: "Avatar" }, as: :json, headers: { "HTTP_AUTHORIZATION" => "Token token=#{@user}" }
     end
 
     assert_response 201
@@ -25,7 +33,7 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update movie" do
-    patch movie_url(@movie), params: { movie: { age_classification: @movie.age_classification, director: @movie.director, genre: @movie.genre, launch_date: @movie.launch_date, length: @movie.length, poster: @movie.poster, rating_avg: @movie.rating_avg, synopsis: @movie.synopsis, title: @movie.title } }, as: :json, headers: { "HTTP_AUTHORIZATION" => "Token token=#{@user}" }
+    patch movie_url(@movie), params: { age_classification: @movie.age_classification, director: @movie.director, genre: @movie.genre, launch_date: @movie.launch_date, length: @movie.length, poster: @movie.poster, rating_avg: @movie.rating_avg, synopsis: @movie.synopsis, title: @movie.title }, as: :json, headers: { "HTTP_AUTHORIZATION" => "Token token=#{@user}" }
     assert_response 200
   end
 
