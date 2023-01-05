cd894a76f37b547e709240dd4ad5063befab0403
Victor Rubia <victorrubia@correo.ugr.es>
2023-01-05 17:00:21 +0100

    Añado ruta para la funcionalidad de comprar entrada [skip ci] close #63

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :purchases
   devise_for :users, controllers: { sessions: :user_sessions },
              path_names: { sign_in: :login }
   resource :user, only: [:show, :update]
   resources :movies
   resources :sessions
+  resources :purchases
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -11,4 +13,5 @@ Rails.application.routes.draw do
   #
   # root "movies#index"
   get 'cartelera', to: 'sessions#get_cartelera'
+  post '/sessions/:id/comprar', to: 'sessions#comprar_entrada'
 end
