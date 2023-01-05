0c83ff6c14a364d80aba28b598f8ddfdd3d74860
Victor Rubia <victorrubia@correo.ugr.es>
2023-01-05 13:19:44 +0100

    Añado configuración de rails para los logs [skip ci] close #56

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,6 @@ module MyApi
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
-
     # config.api_only = true
   end
 end
