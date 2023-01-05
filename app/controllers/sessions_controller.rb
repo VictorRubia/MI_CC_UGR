7115657df554047e8a59e347ebb29df0fba23de0
Victor Rubia <victorrubia@correo.ugr.es>
2023-01-05 16:59:45 +0100

    Modifico controlador y vistas de sesion para mostrar entradas[skip ci] close #62 #66

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
-  before_action :set_session, only: %i[ show update destroy ]
+  before_action :set_session, only: %i[ show update destroy comprar_entrada ]
   before_action :prepare_get_cartelera, only: %i[ get_cartelera ]
-  before_action :authenticate_user!, except: %i[ get_cartelera ]
+  before_action :authenticate_user!, except: %i[ get_cartelera comprar_entrada ]
 
   # GET /sessions.json
   def index
@@ -13,6 +13,24 @@ class SessionsController < ApplicationController
   end
 
   def get_cartelera
+  end
+
+  def comprar_entrada
+    # If email and num_tickets are not nil, then create a new purchase and update num_tickets for this session
+    # then render the purchase
+    if params[:email] != nil && params[:num_tickets] != nil
+      @purchase = Purchase.new(email: params[:email],
+                               num_tickets: params[:num_tickets],
+                               price: params[:num_tickets].to_i*@session.price,
+                               timestamp: Time.now.strftime('%d/%m/%Y %H:%M'),
+                               session_id: @session.id)
+      @session.num_tickets -= params[:num_tickets].to_i
+      @session.save
+      @purchase.save
+      render json: @purchase, status: :created
+    else
+      render json: 'Error', status: :unprocessable_entity
+    end
   end
 
   # POST /sessions.json
@@ -38,6 +56,7 @@ class SessionsController < ApplicationController
 
   # DELETE /sessions/1.json
   def destroy
+    Purchase.find_by(session_id: @session.id).destroy if Purchase.find_by(session_id: @session.id).present?
     @session.destroy
   end
 
@@ -49,8 +68,8 @@ class SessionsController < ApplicationController
 
     def prepare_get_cartelera
       if params[:dates] == nil
-        @session = Session.where(dates: Time.now.strftime("%d/%m/%Y").to_s)
-        @movie = Movie.where(id: Session.where(dates: Time.now.strftime("%d/%m/%Y").to_s).select(:movie_id))
+        @session = Session.where(dates: Time.now.strftime('%d/%m/%Y').to_s)
+        @movie = Movie.where(id: Session.where(dates: Time.now.strftime('%d/%m/%Y').to_s).select(:movie_id))
       else
         @session = Session.where(dates: params[:dates])
         @movie = Movie.where(id: Session.where(dates: params[:dates]).select(:movie_id))
@@ -59,6 +78,6 @@ class SessionsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def session_params
-      params.require(:session).permit(:movie_id, :times, :dates)
+      params.require(:session).permit(:movie_id, :times, :dates, :num_tickets, :price, :email)
     end
 end
--- app/views/sessions/_session.json.jbuilder
+++ app/views/sessions/_session.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! session, :id, :movie_id, :times, :dates, :created_at, :updated_at
+json.extract! session, :id, :movie_id, :times, :dates, :num_tickets, :price, :created_at, :updated_at
 json.url session_url(session, format: :json)
